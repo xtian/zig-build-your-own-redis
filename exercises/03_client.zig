96e2b28eb2f58ebc364e06ddede52a73911314ab
Christian Wesselhoeft <hi@xtian.us>
2023-02-09 10:31:33 -0700

    03_client.zig, 03_server.zig: Improve error handling

--- exercises/03_client.zig
+++ exercises/03_client.zig
@@ -5,15 +5,15 @@ const os = std.os;
 
 pub fn main() !void {
     const fd = try os.socket(os.AF.INET, os.SOCK.STREAM, 0);
+    defer os.close(fd);
 
     const addr = os.sockaddr.in{ .addr = 0, .port = 1234 };
-
     try os.connect(fd, @ptrCast(*const os.sockaddr, &addr), @sizeOf(os.sockaddr.in));
+
     _ = try os.write(fd, "hello");
 
     var read_buffer = [_]u8{0} ** 64;
     _ = try os.read(fd, &read_buffer);
 
     std.debug.print("server says: {s}\n", .{read_buffer});
-    os.close(fd);
 }
--- exercises/03_server.zig
+++ exercises/03_server.zig
@@ -5,25 +5,29 @@ const os = std.os;
 
 pub fn main() !void {
     const fd = try os.socket(os.AF.INET, os.SOCK.STREAM, 0);
+    defer os.close(fd);
+
     try os.setsockopt(fd, os.SOL.SOCKET, os.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
 
     const addr = os.sockaddr.in{ .addr = 0, .port = 1234 };
-
     try os.bind(fd, @ptrCast(*const os.sockaddr, &addr), @sizeOf(os.sockaddr.in));
     try os.listen(fd, 1024);
 
     while (true) {
         const conn_fd = os.accept(fd, null, null, 0) catch continue;
+        defer os.close(conn_fd);
 
-        try do_something(conn_fd);
-        os.close(conn_fd);
+        do_something(conn_fd);
     }
 }
 
-fn do_something(conn_fd: os.socket_t) !void {
+fn do_something(conn_fd: os.socket_t) void {
     var read_buffer = [_]u8{0} ** 64;
-    _ = try os.read(conn_fd, &read_buffer);
+    _ = os.read(conn_fd, &read_buffer) catch {
+        std.debug.print("read error", .{});
+        return;
+    };
 
     std.debug.print("client says: {s}\n", .{read_buffer});
-    _ = try os.write(conn_fd, "world");
+    _ = os.write(conn_fd, "world") catch {};
 }
